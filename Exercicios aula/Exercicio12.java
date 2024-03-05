4d6a69d3a088cfc15164c8e80764e102be9e7bcb
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2024-03-05 19:34:00 -0300

    Update Exercicio12.java

--- Exercicios aula/Exercicio12.java
+++ Exercicios aula/Exercicio12.java
@@ -1,24 +1,34 @@
 import java.util.Scanner;
 public class Exercicio12 {
-    public static void main(String[] args){
-        Scanner leitor = new Scanner(System.in);
+    static Scanner leitor;
 
-        System.out.print("Insira a quantidade de votos do candidato C1: ");
-        int votos_c1 = leitor.nextInt();
+    public static void entradaAbrir(){
+        leitor = new Scanner(System.in);
+    }
 
-        System.out.print("Insira a quantidade de votos do candidato C2: ");
-        int votos_c2 = leitor.nextInt();
-
-        System.out.print("Insira a quantidade de votos do candidato C3: ");
-        int votos_c3 = leitor.nextInt();
-
-        System.out.print("insira a quantidade de votos brancos: ");
-        int qnt_brancos = leitor.nextInt();
-
-        System.out.print("Insira a quantidade de votos nulos: ");
-        int qnt_nulos = leitor.nextInt();
-
+    public static void entradaFechar(){
         leitor.close();
+    }
+
+    public static int entradaInt(String mensagem){
+        System.out.print(mensagem);
+        return leitor.nextInt();
+    }
+
+    public static void saidaString(String mensagem){
+        System.out.print(mensagem);
+    }
+
+    public static void main(String[] args){
+        entradaAbrir();
+        
+        int votos_c1 = entradaInt("Insira a quantidade de votos do candidato C1: ");
+        int votos_c2 = entradaInt("Insira a quantidade de votos do candidato C2: ");
+        int votos_c3 = entradaInt("Insira a quantidade de votos do candidato C3: ");
+        int qnt_brancos = entradaInt("insira a quantidade de votos brancos: ");
+        int qnt_nulos = entradaInt("Insira a quantidade de votos nulos: ");
+
+        entradaFechar();
 
         int total_votos = (votos_c1 + votos_c2 + votos_c3 + qnt_brancos + qnt_nulos);
         System.out.println("O total de votos é: " + total_votos);
@@ -26,7 +36,7 @@ public class Exercicio12 {
         double porct_brancos = (qnt_brancos*100)/total_votos;
         double porct_nulos = (qnt_nulos*100)/total_votos;
 
-        System.out.println("A porcentagem de votos brancos é: " + porct_brancos);
-        System.out.println("A porcentagem de votos nulos é: " + porct_nulos);
+        saidaString("A porcentagem de votos brancos é: " + porct_brancos);
+        saidaString("A porcentagem de votos nulos é: " + porct_nulos);
     }
 }
