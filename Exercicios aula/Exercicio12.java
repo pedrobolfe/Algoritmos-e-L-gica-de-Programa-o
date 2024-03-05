3388248913eda742d9e213e1e0d84f2840bc1c50
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2024-03-05 20:27:02 -0300

    Update Exercicio12.java

--- Exercicios aula/Exercicio12.java
+++ Exercicios aula/Exercicio12.java
@@ -16,12 +16,12 @@ public class Exercicio12 {
     }
 
     public static void saidaString(String mensagem){
-        System.out.print(mensagem);
+        System.out.println(mensagem);
     }
 
     public static void main(String[] args){
         int votos_c1, votos_c2, votos_c3, qnt_brancos, qnt_nulos, total_votos;
-        float porct_brancos, porct_nulos;
+        float porct_c1, porct_c2, porct_c3, porct_brancos, porct_nulos;
 
         entradaAbrir();
         
@@ -34,11 +34,19 @@ public class Exercicio12 {
         entradaFechar();
 
         total_votos = (votos_c1 + votos_c2 + votos_c3 + qnt_brancos + qnt_nulos);
-        porct_brancos = (qnt_brancos*100)/total_votos;
-        porct_nulos = (qnt_nulos*100)/total_votos;
 
-        saidaString("O total de votos é: " + total_votos);
-        saidaString("A porcentagem de votos brancos é: " + porct_brancos);
-        saidaString("A porcentagem de votos nulos é: " + porct_nulos);
+        porct_c1 = ((float) votos_c1 * 100) / total_votos;
+        porct_c2 = ((float) votos_c2 * 100) / total_votos;
+        porct_c3 = ((float) votos_c3 * 100) / total_votos;
+
+        porct_brancos = ((float)qnt_brancos*100)/total_votos;
+        porct_nulos = ((float)qnt_nulos*100)/total_votos;
+
+        saidaString("\nO total de votos é: " + total_votos);
+        saidaString("A porcentagem de votos do candidato 1 é: " + porct_c1 + "%");
+        saidaString("A porcentagem de votos do candidato 2 é: " + porct_c2 + "%");
+        saidaString("A porcentagem de votos do candidato 3 é: " + porct_c3 + "%");
+        saidaString("A porcentagem de votos brancos é: " + porct_brancos + "%");
+        saidaString("A porcentagem de votos nulos é: " + porct_nulos + "%");
     }
 }
