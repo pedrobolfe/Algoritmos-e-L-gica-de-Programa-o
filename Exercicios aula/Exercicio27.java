b6ec1d7c4fb0f9a7e089393ad29f92a2709e68cc
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2024-03-12 20:20:32 -0300

    Create Exercicio27.java

---
+++ Exercicios aula/Exercicio27.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Exercicio27 {
+    static Scanner leitor;
+
+    public static void entradaAbrir(){
+        leitor = new Scanner(System.in);
+    }
+
+    public static void entradaFechar(){
+        leitor.close();
+    }
+
+    public static int entradaInt(String mensagem){
+        System.out.print(mensagem);
+        return leitor.nextInt();
+    }
+    public static float entradaDouble(String mensagem){
+        System.out.print(mensagem);
+        return leitor.nextFloat();
+    }
+
+
+    public static void main(String[] args) {
+        double imc, peso, altura;
+        entradaAbrir();
+
+        peso = entradaInt("Peso: ");
+        altura = entradaDouble("Altura: ");
+
+        entradaFechar();
+
+        imc = peso/Math.pow(altura, 2);
+
+        System.out.printf("IMC: %.2f%n",  imc);
+    }
+}
